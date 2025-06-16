92a5d5edbd651bf76eafe306a4fe6e5ce0210602
bharatbhagaur <bharatbhagaur123@gmail.com>
2025-06-16 10:01:29 +0530

    Bharatsingh ,add ArrayCopy

---
+++ Core-Java/Arrays/ArrayCopy.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ArrayCopy {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+        int[] flatArray = new int[rows * cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        int index = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                flatArray[index] = matrix[i][j];
+                index++;
+            }
+        }
+
+        System.out.println("1D array:");
+        for (int i = 0; i < flatArray.length; i++) {
+            System.out.println(flatArray[i]);
+        }
+    }
+}
+
