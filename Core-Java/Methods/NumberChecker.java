e24935fb361ad5e38567cdf9b0b4ee7eb83f6f88
bharatbhagaur <bharatbhagaur123@gmail.com>
2025-06-18 10:43:03 +0530

    BharatSingh , add NumberChecker

---
+++ Core-Java/Methods/NumberChecker.java
@@ -0,0 +1,17 @@
+class NumberChecker {
+    public static int countDigits(int num) {
+        return String.valueOf(num).length();
+    }
+
+    public static int[] storeDigits(int num) {
+        String s = String.valueOf(num);
+        int[] digits = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) digits[i] = s.charAt(i) - '0';
+        return digits;
+    }
+
+    public static boolean isDuck(int[] digits) {
+        for (int i = 1; i < digits.length; i++) {
+            if (digits[i] == 0) return true;
+        }
+}
