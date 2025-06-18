aaf94db79fc1a4bc2cc65b2455fe4ef5e9eb7b81
bharatbhagaur <bharatbhagaur123@gmail.com>
2025-06-18 10:47:22 +0530

    Bharatsingh , add MatrixOperations

---
+++ Core-Java/Methods/MatrixOperations.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class MatrixOperations {
+
+    public static int[][] generateRandomMatrix(int rows, int cols) {
+        int[][] matrix = new int[rows][cols];
+        Random rand = new Random();
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                matrix[i][j] = rand.nextInt(10);
+        return matrix;
+    }
+
+    public static int[][] addMatrices(int[][] a, int[][] b) {
+        int rows = a.length, cols = a[0].length;
+        int[][] result = new int[rows][cols];
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                result[i][j] = a[i][j] + b[i][j];
+        return result;
+    }
+
+    public static int[][] subtractMatrices(int[][] a, int[][] b) {
+        int rows = a.length, cols = a[0].length;
+        int[][] result = new int[rows][cols];
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                result[i][j] = a[i][j] - b[i][j];
+        return result;
+    }
+
+    public static int[][] multiplyMatrices(int[][] a, int[][] b) {
+        int r1 = a.length, c1 = a[0].length, c2 = b[0].length;
+        int[][] result = new int[r1][c2];
+        for (int i = 0; i < r1; i++)
+            for (int j = 0; j < c2; j++)
+                for (int k = 0; k < c1; k++)
+                    result[i][j] += a[i][k] * b[k][j];
+        return result;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) System.out.print(val + " ");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] m1 = generateRandomMatrix(2, 2);
+        int[][] m2 = generateRandomMatrix(2, 2);
+
+        System.out.println("Matrix A:"); printMatrix(m1);
+        System.out.println("Matrix B:"); printMatrix(m2);
+
+        System.out.println("Addition:"); printMatrix(addMatrices(m1, m2));
+        System.out.println("Subtraction:"); printMatrix(subtractMatrices(m1, m2));
+        System.out.println("Multiplication:"); printMatrix(multiplyMatrices(m1, m2));
+    }
+}
+
