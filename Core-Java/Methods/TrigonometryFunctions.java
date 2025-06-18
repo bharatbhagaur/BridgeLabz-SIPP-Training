c1a5d5fb81d588ec9f47ee6947d1958a74f503ac
bharatbhagaur <bharatbhagaur123@gmail.com>
2025-06-18 09:03:08 +0530

    Bharatsingh , add TrigonometryFunctions

---
+++ Core-Java/Methods/TrigonometryFunctions.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TrigonometryFunctions {
+    public static double[] calculateTrigonometricFunctions(double angleInDegrees) {
+        double radians = Math.toRadians(angleInDegrees);
+        double[] result = new double[3];
+        result[0] = Math.sin(radians);
+        result[1] = Math.cos(radians);
+        result[2] = Math.tan(radians);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter angle in degrees: ");
+        double angle = sc.nextDouble();
+
+        double[] trigValues = calculateTrigonometricFunctions(angle);
+        System.out.println("Sine: " + trigValues[0]);
+        System.out.println("Cosine: " + trigValues[1]);
+        System.out.println("Tangent: " + trigValues[2]);
+    }
+}
